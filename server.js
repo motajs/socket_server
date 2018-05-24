33f2128d3b75805c797c9cd42fd85c2b7f653565
oc <ckcz123@126.com>
2018-05-25 01:20:56 +0800

    fix bug

--- server.js
+++ server.js
@@ -144,7 +144,8 @@ wuziqi.on('connection', function (socket) {
         Object.keys(socket.rooms).forEach(function (id) {
             // wuziqi.in(id).emit('error', '对方断开了链接');
             var room = wuziqi.adapter.rooms[id];
-            console.log("R"+id+" disconnect: "+socket.id);
+            if (id!=socket.id)
+                console.log("R"+id+" disconnect: "+socket.id);
             if (isset(room) && isset(room.first) && isset(room.second)) {
                 if (room.first==socket.id || room.second==socket.id) {
                     wuziqi.in(id).emit('error', '对方断开了连接');
